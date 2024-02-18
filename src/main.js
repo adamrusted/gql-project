ca76c427e56be5b637090cfc800325acec3923ab
Adam Rusted <hello@adamrusted.me>
2024-02-18 15:48:45 +0000

    Bug-hunting

--- src/main.js
+++ src/main.js
@@ -5,43 +5,43 @@ const run = async () => {
   try {
     const token = core.getInput("repo_token", { required: true });
     const project = core.getInput("project", { required: true });
-    const org =
-      core.getInput("org") || github.context.payload.repository.owner.login;
+    const org = core.getInput("org");
     const field = core.getInput("field_name", { required: true });
     const option = core.getInput("option_name", { required: true });
-    const client = github.getOctokit(token);
-    console.log(github.context.payload.pull_request.number);
-    const getProject = await client.graphql(
-      `query GetProject {
-  organization(login: ${org}) {
-    projectV2(number: ${project}) {
-      fields(first: 20) {
-        nodes {
-          ... on ProjectV2Field {
-            id
-            name
-          }
-          ... on ProjectV2SingleSelectField {
-            id
-            name
-            options {
-              id
-              name
-            }
-          }
-        }
-      }
-    }
-  }
-}`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    console.log(getProject);
+    console.log(token, project, org, field, option);
+    //     const client = github.getOctokit(token);
+    //     console.log(github.context.payload.pull_request.number);
+    //     const getProject = await client.graphql(
+    //       `query GetProject {
+    //   organization(login: ${org}) {
+    //     projectV2(number: ${project}) {
+    //       fields(first: 20) {
+    //         nodes {
+    //           ... on ProjectV2Field {
+    //             id
+    //             name
+    //           }
+    //           ... on ProjectV2SingleSelectField {
+    //             id
+    //             name
+    //             options {
+    //               id
+    //               name
+    //             }
+    //           }
+    //         }
+    //       }
+    //     }
+    //   }
+    // }`,
+    //       {
+    //         headers: {
+    //           Authorization: `Bearer ${token}`,
+    //           "Content-Type": "application/json",
+    //         },
+    //       }
+    //     );
+    //     console.log(getProject);
   } catch (err) {
     core.setFailed(err.message);
   }
