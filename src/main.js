dff3504d6fa5d7948f5af11d9d045a101e38dd32
Adam Rusted <hello@adamrusted.me>
2024-02-18 15:41:13 +0000

    Update action

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const github = require("@actions/github");
 const run = async () => {
   try {
     const token = core.getInput("repo-token", { required: true });
-    const project = Number(core.getInput("project", { required: true }));
+    const project = core.getInput("project", { required: true });
     const org =
       core.getInput("org") || github.context.payload.repository.owner.login;
     const field = core.getInput("field_name", { required: true });
@@ -12,31 +12,29 @@ const run = async () => {
     const client = github.getOctokit(token);
     console.log(github.context.payload.pull_request.number);
     const getProject = await client.graphql(
-      `query GetProject($Org: String!, $Proj: Int!){
-                  organization(login: $Org){
-                    projectV2(number: $Proj) {
-                      fields(first:20){
-                        nodes {
-                          ... on ProjectV2Field {
-                            id
-                            name
-                          }
-                          ... on ProjectV2SingleSelectField {
-                            id
-                            name
-                            options {
-                              id
-                              name
-                            }
-                          }
-                        }
-                      }
-                    }
-                  }
-                }`,
+      `query GetProject {
+  organization(login: ${org}) {
+    projectV2(number: ${project}) {
+      fields(first: 20) {
+        nodes {
+          ... on ProjectV2Field {
+            id
+            name
+          }
+          ... on ProjectV2SingleSelectField {
+            id
+            name
+            options {
+              id
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+}`,
       {
-        Org: org,
-        Proj: project,
         headers: {
           Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
