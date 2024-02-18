5268fdfd7ed66561fb2902eab106079ef1fae714
Adam Rusted <hello@adamrusted.me>
2024-02-18 14:33:38 +0000

    Slight tweak

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const github = require("@actions/github");
 const run = async () => {
   try {
     const token = core.getInput("repo-token", { required: true });
-    // const client = github.getOctokit(token);
+    const client = github.getOctokit(token);
     console.log(github.context);
   } catch (err) {
     core.setFailed(err.message);
