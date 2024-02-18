382f341d18669221af66089b45070c04918b30f1
Adam Rusted <hello@adamrusted.me>
2024-02-18 15:16:02 +0000

    Try this

--- src/main.js
+++ src/main.js
@@ -5,17 +5,14 @@ const run = async () => {
   try {
     const token = core.getInput("repo-token", { required: true });
     const project = core.getInput("project", { required: true });
-    const select = core.getInput("select_name", { required: true });
-    const column = core.getInput("column_name", { required: true });
+    const org =
+      core.getInput("org") || github.context.payload.repository.owner.login;
+    const field = core.getInput("field_name", { required: true });
+    const option = core.getInput("option_name", { required: true });
     const client = github.getOctokit(token);
     console.log(github.context.payload.pull_request.number);
-    const getProject = await fetch(``, {
-      headers: {
-        Authorization: `Bearer`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query: `query GetProject($Org: String!, $Proj: Int!){
+    const getProject = await client.graphql(
+      `query GetProject($Org: String!, $Proj: Int!){
                   organization(login: $Org){
                     projectV2(number: $Proj) {
                       fields(first:20){
@@ -37,12 +34,15 @@ const run = async () => {
                     }
                   }
                 }`,
-        variables: {
-          Proj: project,
-          Org: github.context.payload.repository.owner.login,
+      {
+        Org: org,
+        Proj: project,
+        headers: {
+          Authorization: `Bearer`,
+          "Content-Type": "application/json",
         },
-      }),
-    }).then((res) => res.json());
+      }
+    );
     console.log(getProject);
   } catch (err) {
     core.setFailed(err.message);
